dc579db079911c1fc00d7fb2f3d214fcdcf1955e
Super Zombi <rost201541@gmail.com>
2023-09-04 16:15:55 +0300

    update server

--- server/server.py
+++ server/server.py
@@ -2,88 +2,95 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import genshin
 import datetime
 import time
+from models import *
+from tools import *
 
 app = FastAPI(title="Genshin Resin API",
     description="Adds information about original resin to hoyolab",
-    version="1.1.0",
+    version="2.0.0",
     contact={
         "name": "GitHub",
         "url": "https://github.com/SuperZombi/genshin-resin-api"
     })
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_methods=["*"],allow_headers=["*"],allow_credentials=True)
 
 
 @app.get("/", response_class=HTMLResponse, tags=["Routes"])
 def home():
     return "<a href='https://github.com/SuperZombi/genshin-resin-api' style='text-align: center; display: block; font-size: 14pt;'>GitHub</a>"
 
-
-class Status(BaseModel):
-    online: bool
-    time: int = int(time.time())
-
 @app.get("/status", response_model=Status, tags=["Routes"])
 def status():
-    return {"online": True, "time": int(time.time())}
+    return {"online": True, "time": int(time.time()), "version": app.version}
 
 
-class GetOriginalResin(BaseModel):
-    nickname: str
-    uid: int
-    current_resin: int = 0
-    max_resin: int = 160
-    next_recover: int = 480
-    next_recover_str: str = str(datetime.timedelta(seconds=480))
-    full_recover: int = 76800
-    full_recover_str: str = str(datetime.timedelta(seconds=76800))
-    
+@app.get("/getUserRegions", response_model=GetUserRegions,
+    tags=["Routes"],
+    description="Get information about user servers",
+    responses={424: Argument_Incorrect}
+)
+async def get_User_Regions(ltuid: int, ltoken: str):
+    try:
+        client = login(ltuid, ltoken)
+        accounts = await get_accounts(client)
+        user_regions = set(filter(lambda x: x,
+            map(lambda x: GenshinServer(x.server).name if x.server in GenshinServer._value2member_map_.keys() else None
+            , accounts)
+        ))
+        return {"user_regions": user_regions}
+    except:
+        raise HTTPException(status_code=424, detail="Argument values are incorrect")
+
+
 @app.get("/getOriginalResin", response_model=GetOriginalResin,
     tags=["Routes"],
     description="Get information about user original resin",
-    responses={
-        424: {
-            "description": "Argument values are incorrect",
-            "content": {
-                "application/json": {
-                    "example": {"detail": "Argument values are incorrect"}
-                }
-            }
-        }
-    }
+    responses={424: Argument_Incorrect}
 )
-async def get_Original_Resin(ltuid: int, ltoken: str):
+async def get_Original_Resin(ltuid: int, ltoken: str, prefer_region: str = None):
     try:
-        cookies = {"ltuid": ltuid, "ltoken": ltoken,
-                   "ltuid_v2": ltuid, "ltoken_v2": ltoken}
-        client = genshin.Client(cookies)
+        client = login(ltuid, ltoken)
+        accounts = await get_accounts(client)
+        account = get_account(accounts, prefer_region)
+        account_info = get_account_info(account, accounts)
 
-        accounts = await client.get_game_accounts()
-        account = accounts[0]
-        uid = account.uid
+        notes = await client.get_notes(account.uid)
+        next_recover = notes.remaining_resin_recovery_time.total_seconds() % 480
 
-        notes = await client.get_notes(uid)
-        next_recover = notes.remaining_resin_recovery_time.seconds % 480
-
-        return {
-            "nickname": account.nickname,
-            "uid": account.uid,
+        return account_info | {
             "current_resin": notes.current_resin,
-            "max_resin": 160,
+            "max_resin": notes.max_resin,
             "next_recover": next_recover,
             "next_recover_str": str(datetime.timedelta(seconds=next_recover)),
-            "full_recover": notes.remaining_resin_recovery_time.seconds,
+            "full_recover": notes.remaining_resin_recovery_time.total_seconds(),
             "full_recover_str": str(notes.remaining_resin_recovery_time)
+        }
+    except:
+        raise HTTPException(status_code=424, detail="Argument values are incorrect")
+
+
+@app.get("/getRealmCurrency", response_model=GetRealmCurrency,
+    tags=["Routes"],
+    description="Get information about user realm currency",
+    responses={424: Argument_Incorrect}
+)
+async def get_Realm_Currency(ltuid: int, ltoken: str, prefer_region: str = None):
+    try:
+        client = login(ltuid, ltoken)
+        accounts = await get_accounts(client)
+        account = get_account(accounts, prefer_region)
+        account_info = get_account_info(account, accounts)
+
+        notes = await client.get_notes(account.uid)
+
+        return account_info | {
+            "current_realm": notes.current_realm_currency,
+            "max_realm": notes.max_realm_currency,
+            "full_recover": notes.remaining_realm_currency_recovery_time.total_seconds(),
+            "full_recover_str": format_timedelta(notes.remaining_realm_currency_recovery_time)
         }
     except:
         raise HTTPException(status_code=424, detail="Argument values are incorrect")
